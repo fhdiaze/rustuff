632c30672dffb8156e1ba424871321f2fd29054c
Fredy Diaz <fhdiaz87@gmail.com>
2023-08-24 09:03:25 +1200

    feat: parse is now working

--- src/aoc/twenty_two/five.rs
+++ src/aoc/twenty_two/five.rs
@@ -11,7 +11,9 @@ fn solve() {
 
   for l in input {
     if let Ok(text) = l {
-      parser::parse_line(&text);
+      let tokens = parser::parse_line(&text);
+
+      println!("{:?}", tokens);
     }
   }
 }
@@ -20,28 +22,61 @@ fn solve() {
 mod parser {
   use regex::Regex;
 
-  pub fn parse() {
-
-  }
-
-  enum Token {
+  #[derive(Debug)]
+  pub enum Token {
     Mark(char),
     Blank,
-    Splitter
-  }
-
-  pub fn parse_line(text: &str) {
-    println!("Line={}", text);
-    let re = Regex::new(r"(?<b> {3})|\[(?<m>.)\]|(?<s> )").unwrap();
-    for m in re.find_iter(text) {
-
-      println!("m={:?}", m.as_str());
+    Digit(char),
+    Move {
+      quantity: usize,
+      from: usize,
+      to: usize,
     }
   }
-}
 
-fn process_line() {
+  pub fn parse_line(text: &str) -> Vec<Token> {
 
+    match text.chars().next() {
+      Some(' ') | Some('[') => parse_marks(text),
+      Some('m') => vec![parse_move(text)],
+      _ => vec![],
+    }
+  }
+
+  pub fn parse_move(text: &str) -> Token {
+    let regex = Regex::new(r"move (?<q>\d*) from (?<f>\d*) to (?<t>\d*)").unwrap();
+    let caps = regex.captures(text).unwrap();
+    let q = caps["q"].parse::<usize>().unwrap();
+    let f = caps["f"].parse::<usize>().unwrap();
+    let t = caps["t"].parse::<usize>().unwrap();
+
+    Token::Move {
+      quantity: q,
+      from: f,
+      to: t
+    }
+  }
+
+  pub fn parse_marks(text: &str) -> Vec<Token> {
+    let mut ti = 1;
+    let mut tokens: Vec<Token> = Vec::with_capacity(10);
+
+    for (i, c) in text.chars().enumerate() {
+      if i == ti {
+        let token = match c {
+          _ if c.is_whitespace() => Token::Blank,
+          m if c.is_alphabetic() => Token::Mark(m),
+          _ => Token::Digit(c)
+        };
+
+        tokens.push(token);
+
+        ti += 4;
+      }
+    }
+
+    tokens
+  }
 }
 
 struct Ship {
@@ -54,10 +89,39 @@ impl Ship {
       stacks: Vec::with_capacity(10)
     }
   }
+
+  fn load(&mut self, stack: usize, mark: char) {
+    self.stacks[stack].push(mark);
+  }
+
+  fn run(&mut self, step: Step) {
+    let marks = self.stacks[step.from].pop(step.quantity);
+    self.stacks[step.to].push_many(marks);
+  }
 }
 
 struct Stack {
-  crates: Vec<char>
+  marks: Vec<char>
+}
+
+impl Stack {
+  fn push(&mut self, mark: char) {
+    self.marks.push(mark);
+  }
+
+  fn push_many(&mut self, mut marks: Vec<char>) {
+    self.marks.append(&mut marks);
+  }
+
+  fn pop(&mut self, quantity: usize) -> Vec<char> {
+    let mut marks = Vec::with_capacity(quantity);
+
+    for _ in 0 .. quantity {
+      marks.push(self.marks.pop().unwrap());
+    }
+
+    marks
+  }
 }
 
 struct Step {
