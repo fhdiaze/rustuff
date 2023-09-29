836c8c304e196607fe8e0cb5355ce7f87648d586
Fredy Diaz <fhdiaz87@gmail.com>
2023-09-29 18:21:31 +1300

    feat: add day six

--- src/aoc/twenty_two/five.rs
+++ src/aoc/twenty_two/five.rs
@@ -1,178 +1,171 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead, Lines};
 use relative_path::RelativePath;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
 
 use self::parser::Token;
 
 pub fn main() {
-  solve();
+    solve();
 }
 
 fn solve() {
-  let input = read_input();
-  let mut ship = Ship::new();
+    let input = read_input();
+    let mut ship = Ship::new();
 
-  for l in input {
-    if let Ok(text) = l {
-      let tokens = parser::parse_line(&text);
-      ship.run(tokens);
+    for text in input.flatten() {
+        let tokens = parser::parse_line(&text);
+        ship.run(tokens);
     }
-  }
 
-  println!("{:?}", ship.top().iter().collect::<String>());
+    println!("{:?}", ship.top().iter().collect::<String>());
 }
 
-
 mod parser {
-  use regex::Regex;
+    use regex::Regex;
 
-  #[derive(Debug)]
-  pub enum Token {
-    Mark(char),
-    Blank,
-    Digit(char),
-    Move {
-      quantity: usize,
-      from: usize,
-      to: usize,
-    }
-  }
-
-  pub fn parse_line(text: &str) -> Vec<Token> {
-    match text.chars().next() {
-      Some(' ') | Some('[') => parse_marks(text),
-      Some('m') => vec![parse_move(text)],
-      _ => vec![],
-    }
-  }
-
-  pub fn parse_move(text: &str) -> Token {
-    let regex = Regex::new(r"move (?<q>\d*) from (?<f>\d*) to (?<t>\d*)").unwrap();
-    let caps = regex.captures(text).unwrap();
-    let q = caps["q"].parse::<usize>().unwrap();
-    let f = caps["f"].parse::<usize>().unwrap();
-    let t = caps["t"].parse::<usize>().unwrap();
-
-    Token::Move {
-      quantity: q,
-      from: f,
-      to: t
-    }
-  }
-
-  pub fn parse_marks(text: &str) -> Vec<Token> {
-    let mut ti = 1;
-    let mut tokens: Vec<Token> = Vec::with_capacity(10);
-
-    for (i, c) in text.chars().enumerate() {
-      if i == ti {
-        let token = match c {
-          _ if c.is_whitespace() => Token::Blank,
-          m if c.is_alphabetic() => Token::Mark(m),
-          _ => Token::Digit(c)
-        };
-
-        tokens.push(token);
-
-        ti += 4;
-      }
+    #[derive(Debug)]
+    pub enum Token {
+        Mark(char),
+        Blank,
+        Digit(char),
+        Move {
+            quantity: usize,
+            from: usize,
+            to: usize,
+        },
     }
 
-    tokens
-  }
+    pub fn parse_line(text: &str) -> Vec<Token> {
+        match text.chars().next() {
+            Some(' ') | Some('[') => parse_marks(text),
+            Some('m') => vec![parse_move(text)],
+            _ => vec![],
+        }
+    }
+
+    pub fn parse_move(text: &str) -> Token {
+        let regex = Regex::new(r"move (?<q>\d*) from (?<f>\d*) to (?<t>\d*)").unwrap();
+        let caps = regex.captures(text).unwrap();
+        let q = caps["q"].parse::<usize>().unwrap();
+        let f = caps["f"].parse::<usize>().unwrap();
+        let t = caps["t"].parse::<usize>().unwrap();
+
+        Token::Move {
+            quantity: q,
+            from: f,
+            to: t,
+        }
+    }
+
+    pub fn parse_marks(text: &str) -> Vec<Token> {
+        let mut ti = 1;
+        let mut tokens: Vec<Token> = Vec::with_capacity(10);
+
+        for (i, c) in text.chars().enumerate() {
+            if i == ti {
+                let token = match c {
+                    _ if c.is_whitespace() => Token::Blank,
+                    m if c.is_alphabetic() => Token::Mark(m),
+                    _ => Token::Digit(c),
+                };
+
+                tokens.push(token);
+
+                ti += 4;
+            }
+        }
+
+        tokens
+    }
 }
 
 #[derive(Debug)]
 struct Ship {
-  stacks: Vec<Stack>
+    stacks: Vec<Stack>,
 }
 
 impl Ship {
-  fn new() -> Self {
-    Ship {
-      stacks: Vec::with_capacity(10)
+    fn new() -> Self {
+        Ship {
+            stacks: Vec::with_capacity(10),
+        }
     }
-  }
 
-  fn run(&mut self, tokens: Vec<Token>) {
-    for (index, token) in tokens.iter().enumerate() {
-      match token {
-        Token::Mark(_) => {
-          self.run_load(index, token)
-        },
-        Token::Move { .. } => {
-          self.run_move(token)
-        },
-        _ => ()
-      }
+    fn run(&mut self, tokens: Vec<Token>) {
+        for (index, token) in tokens.iter().enumerate() {
+            match token {
+                Token::Mark(_) => self.run_load(index, token),
+                Token::Move { .. } => self.run_move(token),
+                _ => (),
+            }
+        }
     }
-  }
 
-  fn run_load(&mut self, stack: usize, token: &Token) {
-    if let Token::Mark(m) = token {
-      self.try_add_stack(stack);
-      self.stacks[stack].insert(*m);
+    fn run_load(&mut self, stack: usize, token: &Token) {
+        if let Token::Mark(m) = token {
+            self.try_add_stack(stack);
+            self.stacks[stack].insert(*m);
+        }
     }
-  }
 
-  fn try_add_stack(&mut self, s: usize) {
-    while s >= self.stacks.len() {
-      self.stacks.push(Stack::new());
+    fn try_add_stack(&mut self, s: usize) {
+        while s >= self.stacks.len() {
+            self.stacks.push(Stack::new());
+        }
     }
-  }
 
-  fn run_move(&mut self, m: &Token) {
-    if let Token::Move { quantity, from, to } = m {
-      let marks = self.stacks[from-1].pop(*quantity);
-      self.stacks[to-1].push_many(marks);
+    fn run_move(&mut self, m: &Token) {
+        if let Token::Move { quantity, from, to } = m {
+            let marks = self.stacks[from - 1].pop(*quantity);
+            self.stacks[to - 1].push_many(marks);
+        }
     }
-  }
 
-  fn top(&self) -> Vec<char> {
-    self.stacks.iter().map(|s| s.top()).collect()
-  }
+    fn top(&self) -> Vec<char> {
+        self.stacks.iter().map(|s| s.top()).collect()
+    }
 }
 
 #[derive(Debug)]
 struct Stack {
-  marks: Vec<char>
+    marks: Vec<char>,
 }
 
 impl Stack {
-  fn new() -> Self {
-    Stack { marks: vec![] }
-  }
-  fn insert(&mut self, mark: char) {
-    self.marks.insert(0, mark);
-  }
-
-  fn push(&mut self, mark: char) {
-    self.marks.push(mark);
-  }
-
-  fn push_many(&mut self, mut marks: Vec<char>) {
-    self.marks.append(&mut marks);
-  }
-
-  fn pop(&mut self, quantity: usize) -> Vec<char> {
-    let mut marks = Vec::with_capacity(quantity);
-
-    for _ in 0 .. quantity {
-      marks.push(self.marks.pop().unwrap());
+    fn new() -> Self {
+        Stack { marks: vec![] }
+    }
+    fn insert(&mut self, mark: char) {
+        self.marks.insert(0, mark);
     }
 
-    marks
-  }
+    fn push(&mut self, mark: char) {
+        self.marks.push(mark);
+    }
 
-  fn top(&self) -> char {
-    self.marks.last().copied().unwrap_or(' ')
-  }
+    fn push_many(&mut self, mut marks: Vec<char>) {
+        self.marks.append(&mut marks);
+    }
+
+    fn pop(&mut self, quantity: usize) -> Vec<char> {
+        let mut marks = Vec::with_capacity(quantity);
+
+        for _ in 0..quantity {
+            marks.push(self.marks.pop().unwrap());
+        }
+
+        marks
+    }
+
+    fn top(&self) -> char {
+        self.marks.last().copied().unwrap_or(' ')
+    }
 }
 
 fn read_input() -> Lines<BufReader<File>> {
-  let relative = RelativePath::new("./src/aoc/twenty_two/five.txt");
-  let file = File::open(relative.to_path("./")).unwrap();
-  let buf = BufReader::new(file);
+    let relative = RelativePath::new("./src/aoc/twenty_two/five.txt");
+    let file = File::open(relative.to_path("./")).unwrap();
+    let buf = BufReader::new(file);
 
-  buf.lines()
+    buf.lines()
 }
--- src/aoc/twenty_two/six.rs
+++ src/aoc/twenty_two/six.rs
@@ -1,15 +1,38 @@
-use std::{fs::File, io::BufReader};
+use std::{
+    fs::File,
+    io::BufRead,
+    io::{BufReader, Error, Lines},
+    iter::FlatMap,
+    str::Chars,
+};
 
-fn main() {
-  solve();
+pub fn main() {
+    solve();
 }
 
 fn solve() {
-  let input = read_input();
+    let input = read_input();
+    let mut i = 3;
+
+    while i < input.len() {
+        match input[i] {
+            x if x == input[i - 1] => i += 3,
+            y if y == input[i - 2] => i += 2,
+            z if z == input[i - 3] => i += 1,
+            _ => {
+                break;
+            }
+        }
+    }
+
+    println!("Marker found at: {}", i);
 }
 
-fn read_input() -> String {
-  let file = File::open("input.txt").unwrap();
-  let buf = BufReader::new(file);
-  buf.;
+fn read_input() -> Vec<char> {
+    let file = File::open("./src/aoc/twenty_two/six.txt").unwrap();
+    let buf = BufReader::new(file);
+
+    buf.lines()
+        .flat_map(|l| l.expect("Lines failed").chars().collect::<Vec<char>>())
+        .collect::<Vec<char>>()
 }
